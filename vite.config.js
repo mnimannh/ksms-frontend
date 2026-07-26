ea29e63b5758e8d35bc4b32b5e1a4ec20e1806ba
Muhammad Nor Iman <noriman148@gmail.com>
2026-07-27 00:27:59 +0800

    docker

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true, // Enables hot-reload on Windows mounted volumes
+    },
+  },
 })
